0a4e5285bfb234a6581ee6c1858042427d503b88
Robert Hohman <rhohman@rhohman-workstation>
2016-02-02 17:03:03 -0500

    sorting out classes

--- planet_game.py
+++ planet_game.py
@@ -1,16 +1,70 @@
 #!/usr/bin/python
 
-import os
-import sys
+class Location(object):
+    location = 2
+    planets = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune']
 
-class Planet(object):
+    def __init__(self):
+        self.set_names()
+
+    def set_names(self):
+        self.name = self.planets[self.location]
+        self.description ="This is place is called {}".format(self.name)
+
+    def move(self,num):
+        self.location += num
+        self.set_names()
+
+
+class Planet(Location):
+    
+    def look(self):
+        print self.desc
+
+
+
+
+class Mercury(Planet):
+    desc = "The smallest planet"
+
+class Venus(Planet):
     pass
 
-class Moon(Planet):
+class Earth(Planet):
+    desc = "This planet is Mostly Harmless"
     pass
 
-class Movement(object):
+class Mars(Planet):
     pass
 
-class Map(object):
+class Jupiter(Planet):
+    desc = "The largest of the planets"
+
+class Saturn(Planet):
     pass
+
+class Uranus(Planet):
+    pass
+
+class Neptune(Planet):
+    pass
+
+
+
+### WHEN MOVE IS CALLED WE WANT TO ALSO SET THE APPROPRIATE PLANET CLASS TO THE CURRENT INSTANCE.
+
+# p1 = Location()
+
+# print p1.description
+
+# p1.move(2)
+
+# print p1.description
+
+x = Earth()
+
+print x.description
+
+x.look()
+
+
