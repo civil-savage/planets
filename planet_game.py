d7d47a28d8edc71d73c76bcb18313ccacc6baa92
Robert Hohman <rhohman@rhohman-workstation>
2016-02-03 16:47:53 -0500

    toying around

--- planet_game.py
+++ planet_game.py
@@ -1,53 +1,83 @@
 #!/usr/bin/python
 
-class Location(object):
+
+class Universe_Builder(object):
+
     location = 2
+    locations = ['Sun','Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto','Oort Cloud','Kupier Belt']
     planets = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune']
+    moons = ['Luna', 'Pheobe', 'Io', 'Titan', 'Puck', 'Triton']
 
     def __init__(self):
-        self.set_names()
+        self.set_name()
 
-    def set_names(self):
-        self.name = self.planets[self.location]
+    def set_name(self):
+        self.name = self.locations[self.location]
         self.description ="This is place is called {}".format(self.name)
 
-    def move(self,num):
-        self.location += num
-        self.set_names()
+    def build_planet(self):
+        return  type(str(self.planets[self.location]),(Planet,),dict(desc = "Test"))
 
+    def build_moon(self):
+        return  type(str(self.moon[self.location]),(Moon,),dict(desc = "Test"))
 
-class Planet(Location):
-    
     def look(self):
         print self.desc
 
+    def move(self,num):
+        self.location += num
+        self.set_name()
+        self.build_planet()
 
 
+class Planet(Universe_Builder):
 
-class Mercury(Planet):
-    desc = "The smallest planet"
+    def set_name(self):
+        self.name = self.planets[self.location]
+    
+    def land(self):
+        if self.name = "Earth":
+            
 
-class Venus(Planet):
-    pass
+class Moon(Universe_Builder):
 
-class Earth(Planet):
-    desc = "This planet is Mostly Harmless"
-    pass
+    def set_name(self):
+        self.name = self.moon[self.location]
 
-class Mars(Planet):
-    pass
 
-class Jupiter(Planet):
-    desc = "The largest of the planets"
+i = Universe_Builder()
+bp = i.build_planet()
+p = bp()
 
-class Saturn(Planet):
-    pass
+p.look()
+print p.name
+p.move(-1)
+p.look()
+print p.name
+# class Mercury(Planet):
+#     desc = "The smallest planet"
 
-class Uranus(Planet):
-    pass
+# class Venus(Planet):
+#     pass
 
-class Neptune(Planet):
-    pass
+# class Earth(Planet):
+#     desc = "This planet is Mostly Harmless"
+#     pass
+
+# class Mars(Planet):
+#     pass
+
+# class Jupiter(Planet):
+#     desc = "The largest of the planets"
+
+# class Saturn(Planet):
+#     pass
+
+# class Uranus(Planet):
+#     pass
+
+# class Neptune(Planet):
+#     pass
 
 
 
@@ -61,10 +91,14 @@ class Neptune(Planet):
 
 # print p1.description
 
-x = Earth()
+# x = Earth()
 
-print x.description
+# print x.description
 
-x.look()
+# x.look()
 
+# x.move(-2)
 
+# x.look()
+
+# print x.planets[x.location]
